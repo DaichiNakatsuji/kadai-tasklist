a7cb45cc111421409ba89e11c8456b0b831221aa
Daichi.Nakatsuji <nakatsuji.daichi@trans-cosmos.co.jp>
2025-06-09 14:37:58 +0900

    add validate and column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -40,8 +40,14 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required',
+        ]);
+
         // タスクを作成
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -82,9 +88,14 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required',
+        ]);
+
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
-        // タスクを更新
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
